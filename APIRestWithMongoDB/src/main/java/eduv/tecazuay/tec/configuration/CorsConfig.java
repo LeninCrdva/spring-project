8f4448ca32c24e51a882353f8223c20e612dae9c
LeninCr <lenin.cordova.est@tecazuay.edu.ec>
2024-01-23 03:05:24 -0500

    Create CorsConfig.java

---
+++ APIRestWithMongoDB/src/main/java/eduv/tecazuay/tec/configuration/CorsConfig.java
@@ -0,0 +1,31 @@
+package eduv.tecazuay.tec.configuration;
+
+//Configuración de CORS en una clase Java
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+ @Bean
+ public CorsFilter corsFilter() {
+     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+     CorsConfiguration config = new CorsConfiguration();
+
+     // Permitir solicitudes desde todas las fuentes (ajusta según tus necesidades)
+     config.addAllowedOrigin("*");
+
+     // Permitir solicitudes con los métodos GET, POST, PUT, DELETE, etc.
+     config.addAllowedMethod("*");
+
+     // Permitir solicitudes con los encabezados específicos (ajusta según tus necesidades)
+     config.addAllowedHeader("*");
+
+     source.registerCorsConfiguration("/**", config);
+     return new CorsFilter(source);
+ }
+}
